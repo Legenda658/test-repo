db0620d92ca5f6c294cbe22812305fe32770ed5a
Legenda658 <vdomninsg@mail.ru>
2025-06-10 21:13:24 +0300

    Добавлен новый функционал #187

---
+++ tests/test_feature_187.py
@@ -0,0 +1,14 @@
+import unittest
+from src.feature_187 import calculate_feature_187, validate_feature_187
+class TestFeature187(unittest.TestCase):
+    def test_calculation(self):
+        data = [1, 2, 3, 4, 5]
+        result = calculate_feature_187(data)
+        self.assertEqual(result, [2, 4, 6, 8, 10])
+    def test_validation(self):
+        valid_data = [1, 2, 3]
+        invalid_data = [1, '2', 3]
+        self.assertTrue(validate_feature_187(valid_data))
+        self.assertFalse(validate_feature_187(invalid_data))
+if __name__ == '__main__':
+    unittest.main()
